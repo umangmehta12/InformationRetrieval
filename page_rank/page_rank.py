80a59336ef812dde5d6f5e7d9274273be8e82fb9
Umang Mehta <mehta.u@husky.neu.edu>
2015-10-16 12:39:51 -0400

    SOme changes with page_rank implementation

--- page_rank/page_rank.py
+++ page_rank/page_rank.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from decimal import *
 import math
 
-DAMPING_FACTOR = Decimal(0.85)
+DAMPING_FACTOR = 0.85
 outlinks = defaultdict(set)
 inlinks = defaultdict(set)
 all_pages = set()
@@ -32,23 +32,23 @@ def get_parser():
     return parser
 
 def createlinks(node, pages):
+    if not pages:
+        inlinks[node]
     for page in pages:
-        if not page =='':
-            inlinks[node].add(page)
-            outlinks[page].add(node)
-            if not outlinks.has_key(node):
-                outlinks[node]
+        inlinks[node].add(page)
+        outlinks[page].add(node)
+    if not outlinks.has_key(node):
+            outlinks[node]
 
 def setup_links(line):
-    if len(line) > 0:
-        pages = line.split(" ")
-        node = pages[0]
-        all_pages.add(node)
-        if len(pages) == 1:
+    pages = line.rstrip().split(" ")
+    node = pages[0]
+    all_pages.add(node)
+    if len(pages) == 1:
 #            print "No inlinks for: ", pages
-            source.add(pages[0])
-        else:
-            createlinks(node, pages[1:])
+        source.add(pages[0])
+#    sorted_inlinks[node].add(len(pages)-1)
+    createlinks(node, pages[1:])
 
 def process_file(input_file):
     with open(input_file, "r") as file:
@@ -62,26 +62,23 @@ def get_sink(outlinks):
 
 def initialize_pagerank():
     for page in all_pages:
-        page_rank[page] = Decimal(1)/Decimal(len(all_pages))
+        page_rank[page] = 1.0/len(all_pages)
 
 def calculate_perplexity():
-    entropy = Decimal(0)
+    entropy = 0.0
     for page in page_rank.keys():
-        entropy += page_rank[page] * Decimal(math.log(Decimal(1)/rank, Decimal(2)))
-    return math.pow(2, entropy)
+        entropy += page_rank[page] * math.log((1/page_rank[page]), 2)
+#    entropy = -entropy
+    return 2**entropy
 
 def isconverged(counter):
-    print counter
     perplexity_value = calculate_perplexity()
+    print "Counter ", counter+1,"Perplextiy Value : " ,perplexity_value
     print "Value : ", perplexity_value
     perplexity.append(perplexity_value)
     print len(perplexity)
     if len(perplexity) > 4:
-        first = int(perplexity[counter])
-        second = int(perplexity[counter - 1])
-        third = int(perplexity[counter - 2])
-        fourth = int(perplexity[counter - 3])
-        if (first == second == third == fourth):
+        if (int(perplexity[counter]) == int(perplexity[counter - 1]) == int(perplexity[counter - 2]) == int(perplexity[counter - 3])):
             print counter + 1," ",calculate_perplexity(),"\n"
             return False
         else:
@@ -91,18 +88,18 @@ def isconverged(counter):
 
 def perform_calculations():
     counter = 0
-    temp = (Decimal(1) - DAMPING_FACTOR)/Decimal(len(all_pages))
-#    while isconverged(counter):
-    while counter<=10:
-        sink_page_rank = Decimal(0)
+    temp = (1.0 - DAMPING_FACTOR)/len(all_pages)
+    while isconverged(counter):
+#    while counter<=1:
+        sink_page_rank = 0
         for sink_page in sink:
             sink_page_rank += page_rank[sink_page]
         for page in all_pages:
-            temp1 = (DAMPING_FACTOR * Decimal(sink_page_rank)/Decimal(len(all_pages)))
+            temp1 = DAMPING_FACTOR * sink_page_rank/len(all_pages)
             new_page_rank[page] = temp + temp1
             if inlinks.get(page) is not None:
                 for outlink_page in inlinks.get(page):
-                    new_page_rank[page] += (DAMPING_FACTOR * page_rank[outlink_page])/Decimal(len(outlinks.get(outlink_page)))
+                    new_page_rank[page] += DAMPING_FACTOR * page_rank[outlink_page]/len(outlinks.get(outlink_page))
         for pages in all_pages:
             page_rank[pages] = new_page_rank[pages]
         counter +=1
@@ -121,11 +118,10 @@ def main():
     initialize_pagerank()
     perform_calculations()
     print "Length of inlinks: ", len(inlinks)
-    print "Length of outlinks: ", len(outlinks)
+    print "Length of outlinks: ", len(outlinks) - len(sink)
     print "Length of Nodes : ", len(all_pages)
     print "Length of Sink : ", len(sink)
     print "Length of source : ", len(source)
-    print page_rank
 
 if __name__ == '__main__':
     main()
